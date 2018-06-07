d81e0788b528d1ab73cf4a0236533b30b0dfc86c
David Lobaccaro <davego44@yahoo.com>
2018-06-06 23:13:22 -0600

    changed config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,29 @@
 let path = require('path')
 
 module.exports = {
-    
+    context: path.join(__dirname, "src"),
+    entry: "./client/index.js",
+    mode: "development",
+    module: {
+        rules: [{
+            enforce: "pre",
+            test: /\.js$/,
+            loader: "standard-loader",
+            exclude: /node_modules/
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            query: {
+                presets: ["env", "react"]
+            }
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader?modules,localIdentName=[hash:base64:6]-[name]-[local]"
+        }]
+    },
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    }
 }
